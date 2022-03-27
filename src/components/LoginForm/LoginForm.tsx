24394cbc43891e73f472aa0fbade776efaeccfdd
dp-arhipov <72319745+dp-arhipov@users.noreply.github.com>
2022-03-27 22:53:44 +0300

    feature: add loginForm component

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -4,13 +4,19 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
 const LoginForm: React.FC = () => {
+  const login = () => {
+    console.log('submit');
+  };
+
   return (
     <div className={styles.loginForm}>
-      <TextField label="Логин" variant="outlined" />
-      <TextField label="Пароль" variant="outlined" />
-      <Button className={styles.button} variant="contained" size="large">
-        Login
-      </Button>
+      <form className={styles.form} onSubmit={login}>
+        <TextField label="Логин" variant="outlined" />
+        <TextField label="Пароль" variant="outlined" />
+        <Button className={styles.button} variant="contained" size="large" type="submit">
+          Login
+        </Button>
+      </form>
     </div>
   );
 };
