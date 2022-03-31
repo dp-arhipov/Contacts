1f622c41a2549da2d6c2477b3b34049323efa74f
dp-arhipov <72319745+dp-arhipov@users.noreply.github.com>
2022-03-31 17:32:57 +0300

    feature: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,22 @@ import './App.css';
 import Contacts from './pages/Contacts';
 import Login from './pages/Login';
 
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import RequireAuth from './RequireAuth';
+import NotFound from './pages/NotFound';
+
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<Contacts />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
