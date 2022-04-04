05b393c3f934ccb45365224e19d825c3fdcc65d6
dp-arhipov <72319745+dp-arhipov@users.noreply.github.com>
2022-04-04 16:45:28 +0300

    feature: update NotFound component look

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,16 @@
-import React, {Fragment} from 'react';
+import {Typography} from '@mui/material';
+import React from 'react';
+import CenterXY from '../../components/CenterXY';
 import styles from './notFound.module.scss';
 
 const NotFound: React.FC = () => {
   return (
     <div className={styles.notFound}>
-      <h2>Page not found</h2>{' '}
+      <CenterXY>
+        <Typography variant="h3" component="h2">
+          Sorry, not such page
+        </Typography>
+      </CenterXY>
     </div>
   );
 };
