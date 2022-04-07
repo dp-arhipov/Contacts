3cb3421ce8a1646b60e94eb8e02b64fe36170663
dp-arhipov <72319745+dp-arhipov@users.noreply.github.com>
2022-04-07 21:14:46 +0300

    feature: add mockup api

--- src/store/actions/contactsActions.ts
+++ src/store/actions/contactsActions.ts
@@ -2,8 +2,8 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 import {Contact} from '../../types';
 export const getContacts = createAsyncThunk('contacts/getAll', async () => {
   try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    console.log(response);
+    const response = await fetch('https://624d6c5ed71863d7a8179557.mockapi.io/contacts');
+
     return response.json();
   } catch (e) {
     return e.value;
