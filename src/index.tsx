f55dff963279927eccfe1b4ed1a7c6df0dde161c
dp-arhipov <72319745+dp-arhipov@users.noreply.github.com>
2022-04-08 11:19:23 +0300

    feature: add redux-toolkit-persist library

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
-import {store} from './store';
+import {persistor, store} from './store';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {PersistGate} = require('redux-persist/lib/integration/react');
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
