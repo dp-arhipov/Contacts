a7d19f3d6941236162f0f69d9ad6896a39745bd5
dp-arhipov <72319745+dp-arhipov@users.noreply.github.com>
2022-04-09 11:50:03 +0300

    feature: add local auth data check

--- src/store/actions/userActions.ts
+++ src/store/actions/userActions.ts
@@ -1,12 +1,27 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
-import {AuthData, User} from '../../types';
+import {AuthData} from '../../types';
 
-export const login = createAsyncThunk('user/login', async (data: AuthData) => {
+async function serverLogin(login: string, password: string) {
+  return new Promise(function (resolve, reject) {
+    const testUser = {
+      id: 1,
+      login: 'admin',
+      password: 'admin',
+      name: 'Admin user',
+    };
+    if (login == testUser.login && password == testUser.password) {
+      setTimeout(() => resolve(testUser), 300);
+    } else {
+      setTimeout(() => reject(new Error('NOT_CORRECT_AUTH_DATA')), 300);
+    }
+  });
+}
+
+export const login = createAsyncThunk('user/login', async (data: AuthData, {rejectWithValue}) => {
   try {
-    const {login} = data;
-    const userData: User = {login, name: 'Admin', id: 1};
-    return userData;
+    const responce = await serverLogin(data.login, data.password);
+    return responce;
   } catch (e) {
-    return e.value;
+    return rejectWithValue(e.message);
   }
 });
